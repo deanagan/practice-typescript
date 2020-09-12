b650b076bdc2568ce6cedc733c1289d611debf11
Dean Agan <agandfr@gmail.com>
2020-09-12 20:41:41 +1000

    Fix counting to not reset when we've hit 3 already

--- src/three-words.ts
+++ src/three-words.ts
@@ -1,5 +1,6 @@
 
 
-export function threeWords(texts: string) : boolean {
-    return texts.split(' ').reduce((total, word) => total + (Number.isNaN(+word) ? 1 : -total), 0) >= 3;
+export function threeWords(text: string) : boolean {
+    return text.split(' ')
+        .reduce((total, word) => total + ((Number.isNaN(+word) || total >= 3) ? 1 : -total), 0) >= 3;
 }
