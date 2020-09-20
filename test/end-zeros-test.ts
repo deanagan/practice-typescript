944900b90d1bc7e183c21f03783c0750cc1121f7
Dean Agan <agandfr@gmail.com>
2020-09-20 18:35:56 +1000

    Created unit tests

---
+++ test/end-zeros-test.ts
@@ -0,0 +1,26 @@
+import { endZeros } from "../src/end-zeros";
+import { describe, it } from "mocha";
+
+import { expect } from "chai";
+
+interface TestParameters {
+  input: number;
+  expected: number;
+}
+
+const testParameters : TestParameters[] = [
+  {input: 0, expected: 1},
+  {input: 1, expected: 0},
+  {input: 10, expected: 1},
+  {input: 101, expected: 0},
+  {input: 245, expected: 0},
+  {input: 100100, expected: 2},
+];
+
+testParameters.forEach((tp: TestParameters) => {
+  describe("End Zeros Test", () => {
+    it(`Should return ${tp.expected} when ${tp.input} is inputted.`, () => {
+      expect(endZeros(tp.input)).to.be.equal(tp.expected);
+    });
+  });
+});
