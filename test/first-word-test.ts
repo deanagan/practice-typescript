2000478714e8f8849cafc56381404b1cb253048b
Dean Agan <agandfr@gmail.com>
2020-09-21 18:23:28 +1000

    Create unit test for solution to first word test

---
+++ test/first-word-test.ts
@@ -0,0 +1,26 @@
+import { firstWord } from "../src/first-word";
+import { describe, it } from "mocha";
+
+import { expect } from "chai";
+
+interface TestParameters {
+  input: string;
+  expected: string;
+}
+
+const testParameters : TestParameters[] = [
+    { input: "Hello world", expected: "Hello"},
+    { input: " a word ", expected: "a"},
+    { input: "don't touch it", expected: "don't"},
+    { input: "greetings, friends", expected: "greetings"},
+    { input: "... and so on ...", expected: "and"},
+    { input: "hi", expected: "hi"},
+];
+
+testParameters.forEach((tp: TestParameters) => {
+  describe("First Word Test", () => {
+    it(`Should return "${tp.expected}" when "${tp.input}" is inputted.`, () => {
+      expect(firstWord(tp.input)).to.be.equal(tp.expected);
+    });
+  });
+});
