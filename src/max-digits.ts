c3934c75c5b42e9cb5cd36e3021d8011472c59c6
Dean Agan <agandfr@gmail.com>
2020-09-25 16:43:46 +1000

    Remove comments and export max digit function

--- src/max-digits.ts
+++ src/max-digits.ts
@@ -1,16 +1,6 @@
 
 
-function maxDigit(value: number): number {
+export function maxDigit(value: number): number {
     // Has to be es6. Otherwise downlevel iterator flag for es3/es5 comes to play
     return Number([...value.toString()].sort().pop());
 }
-
-//console.log('Example:');
-//console.log(maxDigit(0));
-
-// These "asserts" are used for self-checking
-//assert.equal(maxDigit(0), 0);
-//assert.equal(maxDigit(52), 5);
-//assert.equal(maxDigit(634), 6);
-//assert.equal(maxDigit(1), 1);
-//assert.equal(maxDigit(10000), 1);
