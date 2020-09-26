5750059a643cffb3f98e82af300487d8c3d9e21e
Dean Agan <agandfr@gmail.com>
2020-09-27 07:43:30 +1000

    Add comment on alternative solution using splice

--- src/split-list.ts
+++ src/split-list.ts
@@ -3,4 +3,7 @@
 export function splitList(values: number[]):number[][] {
     const firstHalf = Math.ceil(values.length / 2);
     return [values.slice(0,firstHalf), values.slice(firstHalf)];
+
+    // Alternatively
+    // return [ values.splice(0,  Math.ceil(values.length/2)), values]
 }
