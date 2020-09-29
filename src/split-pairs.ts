350e9d56f2fbab5b324d7849edd107608e693f01
Dean Agan <agandfr@gmail.com>
2020-09-29 20:01:10 +1000

    Add alternate recursive solution to split pairs

--- src/split-pairs.ts
+++ src/split-pairs.ts
@@ -1,22 +1,15 @@
 
 
 
-function splitPairs(text: string): string[] {
+export function splitPairs(text: string): string[] {
 
-    if (text.length % 2) {
-        text = [...text].concat('_').join('');
-    }
-    // slow but convenient
-    return text.split(/(.{1,2})/).filter(empty => empty);
+    // if (text.length % 2) {
+    //     text = [...text].concat('_').join('');
+    // }
+    // // slow but convenient
+    // return text.split(/(.{1,2})/).filter(empty => empty);
+
+    // Alternatively using recursion
+    let [first, second = '_', ...rest] = text;
+    return !first ? [] : [first+second, ...splitPairs(rest.join(''))];
 }
-
-
-//console.log('Example:');
-//console.log(splitPairs('abcd'));
-
-// These "asserts" are used for self-checking
-// assert.deepEqual(splitPairs('abcd'), ['ab', 'cd']);
-// assert.deepEqual(splitPairs('abc'), ['ab', 'c_']);
-// assert.deepEqual(splitPairs('abcdf'), ['ab', 'cd', 'f_']);
-// assert.deepEqual(splitPairs('a'), ['a_']);
-// assert.deepEqual(splitPairs(''), []);
