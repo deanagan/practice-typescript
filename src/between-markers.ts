7f0e397da5c143f0a5b701e168dc14de9bd786cd
Dean Agan <agandfr@gmail.com>
2020-10-03 15:59:46 +1000

    Removed commented out test and export function

--- src/between-markers.ts
+++ src/between-markers.ts
@@ -1,14 +1,5 @@
 
 
-function betweenMarkers(line: string, left: string, right: string): string {
+export function betweenMarkers(line: string, left: string, right: string): string {
     return line.slice(line.indexOf(left) + 1, line.indexOf(right))
 }
-
-//console.log('Example:');
-//console.log(betweenMarkers('What is >apple<', '>', '<'));
-
-// These "asserts" are used for self-checking
-//assert.equal(betweenMarkers('What is >apple<', '>', '<'), 'apple');
-//assert.equal(betweenMarkers('What is [apple]', '[', ']'), 'apple');
-//assert.equal(betweenMarkers('What is ><', '>', '<'), '');
-//assert.equal(betweenMarkers('[an apple]', '[', ']'), 'an apple');
