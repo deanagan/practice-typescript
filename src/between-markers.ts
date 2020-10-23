ba9959b88b3d2798b02f9987459f205c3e2e903e
Dean Agan <agandfr@gmail.com>
2020-10-23 20:36:09 +1100

    Add solution

--- src/between-markers.ts
+++ src/between-markers.ts
@@ -3,3 +3,10 @@
 export function betweenMarkers(line: string, left: string, right: string): string {
     return line.slice(line.indexOf(left) + 1, line.indexOf(right))
 }
+
+export function betweenWordMarkers(text: string, left: string, right: string): string {
+    let begin = text.indexOf(left) != -1 ? text.indexOf(left) + left.length : 0;
+    let end = text.indexOf(right) != -1 ? text.indexOf(right) : text.length;
+
+    return text.slice(begin, end)
+}
