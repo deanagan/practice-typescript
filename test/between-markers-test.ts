0ea6932d9665dda1ee7556b9e1857cd5edb9fa42
Dean Agan <agandfr@gmail.com>
2020-10-23 20:35:59 +1100

    Add between word markers test

--- test/between-markers-test.ts
+++ test/between-markers-test.ts
@@ -1,4 +1,4 @@
-import { betweenMarkers } from '../src/between-markers';
+import { betweenMarkers, betweenWordMarkers } from '../src/between-markers';
 import { describe, it, Test } from 'mocha';
 import { expect } from 'chai';
 
@@ -22,6 +22,24 @@ describe('Between marker Test', () => {
     testParameters.forEach((tp) => {
         it(`should return '${tp.expected}' when give input '${tp.input}' with left marker '${tp.leftMarker}' and right marker '${tp.rightMarker}'.`, () => {
             expect(betweenMarkers(tp.input, tp.leftMarker, tp.rightMarker)).to.equal(tp.expected);
+        });
+    });
+
+});
+
+const testParameters2 : TestParameter[] = [
+    { input: 'What is >apple<', leftMarker: '>', rightMarker: '<', expected: 'apple'},
+    { input: "<head><title>My new site</title></head>", leftMarker: "<title>", rightMarker: "</title>", expected: 'My new site'},
+    { input: 'No[/b] hi', leftMarker: '[b]', rightMarker: '[/b]', expected: 'No'},
+    { input: 'No [b]hi', leftMarker: '[b]', rightMarker: '[/b]', expected: 'hi'},
+    { input: 'No hi', leftMarker: '[b]', rightMarker: '[/b]', expected: 'No hi'},
+    { input: 'No <hi>', leftMarker: '>', rightMarker: '<', expected: ''},
+];
+
+describe('Between word marker Test', () => {
+    testParameters.forEach((tp) => {
+        it(`should return '${tp.expected}' when give input '${tp.input}' with left marker '${tp.leftMarker}' and right marker '${tp.rightMarker}'.`, () => {
+            expect(betweenWordMarkers(tp.input, tp.leftMarker, tp.rightMarker)).to.equal(tp.expected);
         });
     });
 
