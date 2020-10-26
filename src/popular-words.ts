49f64a579b60034d05288c657fa4943fda588774
Dean Agan <agandfr@gmail.com>
2020-10-26 21:09:00 +1100

    Fix to pass test

--- src/popular-words.ts
+++ src/popular-words.ts
@@ -1,5 +1,17 @@
 
 
+export class LookUp {
+    [key: string]: number;
+}
+
+
 export function popularWords(text: string, words: string[]) {
-    return {};
+    let result : LookUp = new LookUp();
+    const textList = text.toLowerCase().split(/[\s]+/g);
+
+    words.forEach((word) => {
+         result[word] = textList.filter((e) => word == e).length;
+    });
+
+    return result;
 }
