be651c9822508a56cb27d615a0fb7eb4a0b8907b
Dean Agan <agandfr@gmail.com>
2020-10-27 19:07:50 +1100

    Modify code to pass test

--- src/second-index.ts
+++ src/second-index.ts
@@ -1,5 +1,7 @@
 export function secondIndex(text: string, symbol: string): number | undefined {
     let firstIndex = text.indexOf(symbol) + 1;
 
-    return firstIndex > 0 ? text.slice(firstIndex).indexOf(symbol) + firstIndex : undefined;
+    return (firstIndex > 0 && text.lastIndexOf(symbol) != (firstIndex - 1))
+        ? text.slice(firstIndex).indexOf(symbol) + firstIndex
+        : undefined;
 }
