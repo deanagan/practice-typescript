94e6feee2e69df1fefb60b856cebad6b344aeeda
Dean Agan <agandfr@gmail.com>
2020-10-28 19:17:55 +1100

    Solution to pass existing tests

--- src/sort-array-by-element-frequency.ts
+++ src/sort-array-by-element-frequency.ts
@@ -1,3 +1,10 @@
 export function getSortedElementFrequency(items: any[]) : any[] {
-    return [4, 4, 4, 4, 6, 6, 2, 2];
+
+    const occurence = (item: any) => items.filter((e: any) => e == item).length;
+
+    return items.sort((a,b) =>
+        occurence(a) === occurence(b) ?
+            items.indexOf(a) - items.indexOf(b) :
+            occurence(b) - occurence(a)
+    );
 }
