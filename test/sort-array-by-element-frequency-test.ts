33abd393fcb0121f5e7758dfe7269a2603d6d20d
Dean Agan <agandfr@gmail.com>
2020-10-28 18:07:38 +1100

    Created tests

---
+++ test/sort-array-by-element-frequency-test.ts
@@ -0,0 +1,25 @@
+// Sort the given iterable so that its elements end up in the decreasing frequency order, that is, the number of times
+// they appear in elements. If two elements have the same frequency, they should end up in the same order as the first appearance in the iterable.
+
+import { getSortedElementFrequency } from '../src/sort-array-by-element-frequency';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+interface TestParameter {
+    input: any[];
+    expected: any[];
+}
+
+const testParameters : TestParameter[] = [
+    { input: [4, 6, 2, 2, 6, 4, 4, 4], expected: [4, 4, 4, 4, 6, 6, 2, 2]},
+
+];
+
+
+describe("Sorted by frequency test", () => {
+    testParameters.forEach(tp => {
+        it(`should return '${tp.expected}' when given '${tp.input}'`, () => {
+            expect(getSortedElementFrequency(tp.input)).to.be.eql(tp.expected);
+        });
+    });
+});
