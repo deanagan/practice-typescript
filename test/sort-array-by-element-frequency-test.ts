b06011bf7b1db0c754147338d75765eef552bcb0
Dean Agan <agandfr@gmail.com>
2020-10-28 19:19:55 +1100

    Add empty test. Still passes.

--- test/sort-array-by-element-frequency-test.ts
+++ test/sort-array-by-element-frequency-test.ts
@@ -13,7 +13,8 @@ interface TestParameter {
 const testParameters : TestParameter[] = [
     { input: [4, 6, 2, 2, 6, 4, 4, 4], expected: [4, 4, 4, 4, 6, 6, 2, 2]},
     { input: ['bob', 'bob', 'carl', 'alex', 'bob'], expected: ['bob', 'bob', 'bob', 'carl', 'alex']},
-    { input: [17, 99, 42], expected: [17, 99, 42]}
+    { input: [17, 99, 42], expected: [17, 99, 42]},
+    { input: [], expected: []},
 ];
 
 
