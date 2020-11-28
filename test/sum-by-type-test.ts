a36e69d8dc0228b330771f5c606cd3c9f8ca3656
Dean Agan <agandfr@gmail.com>
2020-11-28 15:05:01 +1100

    Add tests

--- test/sum-by-type-test.ts
+++ test/sum-by-type-test.ts
@@ -10,6 +10,11 @@ interface TestParameter {
 
 const testParameters : TestParameter[] = [
     { input: [],  expected: ['', 0]},
+    { input: [1, 2, 3],  expected: ['', 6]},
+    { input: ['1', 2, 3],  expected: ['1', 5]},
+    { input: ['1', '2', 3],  expected: ['12', 3]},
+    { input: ['1', '2', '3'], expected:  ['123', 0]},
+    { input: ['size', 12, 'in', 45, 0], expected:  ['sizein', 57]},
 ];
 
 describe('Sum by types', () => {
