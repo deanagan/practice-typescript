7e47962642398d478ab95d64689c422a16c7f9b3
Dean Agan <agandfr@gmail.com>
2020-11-28 15:04:51 +1100

    Add solution

--- src/sum-by-type.ts
+++ src/sum-by-type.ts
@@ -2,5 +2,16 @@
 
 
 export function sumByType(values: Array<number | string>): [string, number] {
-    return ['', 0];
+
+    return values.reduce((result, e) => {
+        let [text, num] = result;
+
+        if (Number.isInteger(e)) {
+            num += +e;
+        } else {
+            text += e;
+        }
+        return [text, num];
+    }, ['', 0]);
+
 }
