77570b983fe8742866eac807d5c4b2a010425fae
Dean Agan <agandfr@gmail.com>
2020-11-29 16:03:06 +1100

    Alternate solution

--- src/sum-by-type.ts
+++ src/sum-by-type.ts
@@ -3,15 +3,7 @@
 
 export function sumByType(values: Array<number | string>): [string, number] {
 
-    return values.reduce((result, e) => {
-        let [text, num] = result;
-
-        if (Number.isInteger(e)) {
-            num += +e;
-        } else {
-            text += e;
-        }
-        return [text, num];
-    }, ['', 0]);
+    return values.reduce(([num, text], e) =>
+        (e === +e) ? [num, text + e] : [num + e, text], ['', 0]);
 
 }
